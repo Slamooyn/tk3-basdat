b463edad7f794dba90a75ce3ca4c62ce7539da84
Slamooyn <leparhoy@gmail.com>
2026-04-30 21:23:00 +0700

    finaly

---
+++ frontend/app/api/test/route.ts
@@ -0,0 +1,16 @@
+import { pool } from "@/lib/db";
+
+export async function GET() {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    return Response.json(result.rows);
+  } catch (error) {
+    console.error(error);
+
+    return Response.json(
+      { error: "Database connection failed" },
+      { status: 500 }
+    );
+  }
+}
